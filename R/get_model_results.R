6b69e7f717ab80ccb8247afa01bf0d8c574d0733
ardaergin <ardergin@gmail.com>
2023-09-09 02:00:29 +0200

    update to model results

--- R/get_model_results.R
+++ R/get_model_results.R
@@ -11,14 +11,27 @@
 get_model_results <- function(models_list,
                               the_model = "M_LMER_complex"){
 
+  action_options = c(
+    "INACTION",
+    "NORMATIVE ACTION",
+    "NON-NORMATIVE ACTION",
+    "EXTREME NON-NORMATIVE ACTION")
   # For loop for the 4 action options
   for (i in 1:4){
+
+    cat("\n", "\n", "**********", action_options[i], "**********", "\n", "\n")
 
     print(summary(models_list[[i]][[the_model]]))
 
     cat("\n", "\n", "------------------------------", "\n", "\n")
 
     print(stats::anova(models_list[[i]][[the_model]]))
+
+    cat("\n", "\n", "------------------------------", "\n", "\n")
+
+    cat("Marginal (R2m) and Conditional (R2c) R-squared:", "\n", "\n")
+
+    print(MuMIn::r.squaredGLMM(models_list[[i]][[the_model]]))
 
     cat("\n", "\n", "\n", "===========================", "\n", "\n", "\n", "\n", "\n")
   }
