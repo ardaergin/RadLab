84897bbb446bcf342273994c00c7d5f0a37d312d
ardaergin <ardergin@gmail.com>
2023-09-11 10:44:02 +0200

    new pretty summary function

---
+++ R/get_pretty_summaries.R
@@ -0,0 +1,23 @@
+get_pretty_summaries <- function(
+    models_list,
+    action_comparison = TRUE,
+    model_comparison = FALSE
+    ){
+
+  if (action_comparison == TRUE){
+    sjPlot::tab_model(
+      models_list$ina$M_LMER_complex,
+      models_list$na$M_LMER_complex,
+      models_list$nna$M_LMER_complex,
+      models_list$enna$M_LMER_complex)
+  }
+
+  if (model_comparison == TRUE){
+      for(i in 1:4){
+        sjPlot::tab_model(
+        models_list[[i]][["M_LM"]],
+        models_list[[i]][["M_LMER_simple"]],
+        models_list[[i]][["M_LMER_complex"]])
+      }
+  }
+}
