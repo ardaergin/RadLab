ec9b05f78b5ea01fece57dc2b45fba3b49ac62b6
ardaergin <ardergin@gmail.com>
2023-09-11 13:26:59 +0200

    something something

---
+++ R/get_model_comparisons.R
@@ -0,0 +1,40 @@
+
+get_model_comparisons <- function(x){
+  # For some reason, the for loop does not work here
+  
+  cat("\n--------\n")
+  # INA
+  cat(
+    sjPlot::tab_model(
+      models_list[["ina"]][["M_LM"]],
+      models_list[["ina"]][["M_LMER_simple"]],
+      models_list[["ina"]][["M_LMER_complex"]])$knitr,
+    "\n--------\n"
+  )
+  # NA
+  cat(
+    sjPlot::tab_model(
+      models_list[["na"]][["M_LM"]],
+      models_list[["na"]][["M_LMER_simple"]],
+      models_list[["na"]][["M_LMER_complex"]])$knitr,
+    "\n--------\n"
+  )
+  # NNA
+  cat(
+    sjPlot::tab_model(
+      models_list[["nna"]][["M_LM"]],
+      models_list[["nna"]][["M_LMER_simple"]],
+      models_list[["nna"]][["M_LMER_complex"]])$knitr,
+    "\n--------\n"
+  )
+  # ENNA
+  cat(
+    sjPlot::tab_model(
+      models_list[["enna"]][["M_LM"]],
+      models_list[["enna"]][["M_LMER_simple"]],
+      models_list[["enna"]][["M_LMER_complex"]])$knitr,
+    "\n--------\n"
+  )
+}
+
+  
--- R/get_pretty_summaries.R
+++ R/get_pretty_summaries.R
@@ -1,56 +1,12 @@
 get_pretty_summaries <- function(
-    models_list,
-    action_comparison = TRUE,
-    model_comparison = FALSE
+    models_list
     ){
-
-  if (action_comparison == TRUE){
-    cat(
-      sjPlot::tab_model(
-        models_list$ina$M_LMER_complex,
-        models_list$na$M_LMER_complex,
-        models_list$nna$M_LMER_complex,
-        models_list$enna$M_LMER_complex)$knitr,
-      "\n--------\n"
-      )
-  }
-
-  if (model_comparison == TRUE){
-    # For some reason, the for loop does not work here
-
-    cat("\n--------\n")
-    # INA
-    cat(
-      sjPlot::tab_model(
-        models_list[["ina"]][["M_LM"]],
-        models_list[["ina"]][["M_LMER_simple"]],
-        models_list[["ina"]][["M_LMER_complex"]])$knitr,
-      "\n--------\n"
-    )
-    # NA
-    cat(
-      sjPlot::tab_model(
-        models_list[["na"]][["M_LM"]],
-        models_list[["na"]][["M_LMER_simple"]],
-        models_list[["na"]][["M_LMER_complex"]])$knitr,
-      "\n--------\n"
-    )
-    # NNA
-    cat(
-      sjPlot::tab_model(
-        models_list[["nna"]][["M_LM"]],
-        models_list[["nna"]][["M_LMER_simple"]],
-        models_list[["nna"]][["M_LMER_complex"]])$knitr,
-      "\n--------\n"
-    )
-    # ENNA
-    cat(
-      sjPlot::tab_model(
-        models_list[["enna"]][["M_LM"]],
-        models_list[["enna"]][["M_LMER_simple"]],
-        models_list[["enna"]][["M_LMER_complex"]])$knitr,
-      "\n--------\n"
-    )
-
-  }
+  cat(
+    sjPlot::tab_model(
+      models_list$ina$M_LMER_complex,
+      models_list$na$M_LMER_complex,
+      models_list$nna$M_LMER_complex,
+      models_list$enna$M_LMER_complex)$knitr,
+    "\n--------\n"
+  )
 }
