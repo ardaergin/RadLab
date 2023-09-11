9d4a887e9452de24efb5c4d1cba23d1742f69a5b
ardaergin <ardergin@gmail.com>
2023-09-11 10:51:29 +0200

    Update get_pretty_summaries.R

--- R/get_pretty_summaries.R
+++ R/get_pretty_summaries.R
@@ -5,19 +5,26 @@ get_pretty_summaries <- function(
     ){
 
   if (action_comparison == TRUE){
-    sjPlot::tab_model(
-      models_list$ina$M_LMER_complex,
-      models_list$na$M_LMER_complex,
-      models_list$nna$M_LMER_complex,
-      models_list$enna$M_LMER_complex)
+    cat(tab_model(r)$knitr,"\n--------\n")
+    cat(
+      sjPlot::tab_model(
+        models_list$ina$M_LMER_complex,
+        models_list$na$M_LMER_complex,
+        models_list$nna$M_LMER_complex,
+        models_list$enna$M_LMER_complex)$knitr,
+      "\n--------\n"
+      )
   }
 
   if (model_comparison == TRUE){
       for(i in 1:4){
-        sjPlot::tab_model(
-        models_list[[i]][["M_LM"]],
-        models_list[[i]][["M_LMER_simple"]],
-        models_list[[i]][["M_LMER_complex"]])
+        cat(
+          sjPlot::tab_model(
+            models_list[[i]][["M_LM"]],
+            models_list[[i]][["M_LMER_simple"]],
+            models_list[[i]][["M_LMER_complex"]])$knitr,
+          "\n--------\n"
+        )
       }
   }
 }
