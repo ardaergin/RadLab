cbbca43772f4ff0e728bb61f2efcd123a6d7ab7f
ardaergin <ardergin@gmail.com>
2023-09-11 13:32:25 +0200

    Update get_model_comparisons.R

--- R/get_model_comparisons.R
+++ R/get_model_comparisons.R
@@ -7,21 +7,32 @@ get_model_comparisons <- function(models_list){
     sjPlot::tab_model(
       models_list[["ina"]][["M_LM"]],
       models_list[["ina"]][["M_LMER_simple"]],
-      models_list[["ina"]][["M_LMER_complex"]])$knitr
+      models_list[["ina"]][["M_LMER_complex"]])$knitr,
+    "\n"
   )
   # NA
   cat(
     sjPlot::tab_model(
       models_list[["na"]][["M_LM"]],
       models_list[["na"]][["M_LMER_simple"]],
-      models_list[["na"]][["M_LMER_complex"]])$knitr
+      models_list[["na"]][["M_LMER_complex"]])$knitr,
+    "\n"
+  )
+  # NNA
+  cat(
+    sjPlot::tab_model(
+      models_list[["nna"]][["M_LM"]],
+      models_list[["nna"]][["M_LMER_simple"]],
+      models_list[["nna"]][["M_LMER_complex"]])$knitr,
+    "\n"
   )
   # ENNA
   cat(
     sjPlot::tab_model(
       models_list[["enna"]][["M_LM"]],
       models_list[["enna"]][["M_LMER_simple"]],
-      models_list[["enna"]][["M_LMER_complex"]])$knitr
+      models_list[["enna"]][["M_LMER_complex"]])$knitr,
+    "\n"
   )
 }
 
